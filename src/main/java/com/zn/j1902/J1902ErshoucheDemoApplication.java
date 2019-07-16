0b81295276f5daee2fc9ec26fbf8d892ec63cc56
Jusephi <969662816@qq.com>
2019-07-16 21:48:34 +0800

    first

---
+++ src/main/java/com/zn/j1902/J1902ErshoucheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zn.j1902;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J1902ErshoucheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J1902ErshoucheDemoApplication.class, args);
+	}
+
+}
